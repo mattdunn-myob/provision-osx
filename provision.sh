5de75f728eff0326a91fc6822c8aa24f81bb2f6c
Matt Dunn <Matthew.Dunn@myob.com>
2016-08-24 11:25:31 +1000

    add oh-my-zsh

--- provision.sh
+++ provision.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
 # install homebrew and brew installable apps
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew bundle
+
+# install ohm-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
