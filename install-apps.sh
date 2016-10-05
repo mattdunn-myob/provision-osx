80f0be904177f028af31f00cd8252d11c8ea6d23
Matt Dunn <Matthew.Dunn@myob.com>
2016-10-05 11:45:15 +1100

    break out app install into own shellfile, and install pip too

---
+++ install-apps.sh
@@ -0,0 +1,9 @@
+# install homebrew and brew installable apps
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew bundle
+
+# install ohm-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install pip 
+sudo easy_install pip
