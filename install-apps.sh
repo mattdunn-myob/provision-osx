f042558a665ec64e7cbfc04f461a5707d68706ea
Matt Dunn <Matthew.Dunn@myob.com>
2019-05-17 11:01:58 +1000

    remove scm breeze installation

--- install-apps.sh
+++ install-apps.sh
@@ -7,7 +7,3 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 
 # install pip 
 sudo easy_install pip
-
-# install scm_breeze
-git clone git://github.com/ndbroadbent/scm_breeze.git ~/.scm_breeze
-~/.scm_breeze/install.sh
